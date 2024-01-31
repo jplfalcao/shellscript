2d10600940d2728eb06afa98f3a37cd0c13b3375
João Paulo Falcão <jplinfo86@gmail.com>
2024-01-30 22:29:50 -0300

    Updating File

--- scripts/novo-script.sh
+++ scripts/novo-script.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # Autor: João PauLo Falcão
 # Github: https://github.com/jplfalcao
-# Descrição: Gera um arquivo com cabeçalho e permissão de execução
+# Descrição: Cria um arquivo com cabeçalho e permissão de execução
 # Data de criação: 07/11/2021
-# Data de modificação: 10/09/2023
-# Versão: 1.7
+# Data de modificação: 30/01/2024
+# Versão: 1.8
 # Uso: ./novo-script.sh <arquivo>
 
-# Cabeçalho do script
 cabecalho="#!/usr/bin/env bash
 # Autor:
 # Github:
@@ -18,40 +17,32 @@ cabecalho="#!/usr/bin/env bash
 # Uso:
 "
 
-# Verifica se foi passado um nome de arquivo como argumento
 if [ "$#" -ne 1 ]; then
-  echo "Informe um nome para o arquivo!"
-  exit 1
+    echo "Informe um nome para o arquivo!"
+    exit 1
 fi
 
-# Verifica se o arquivo já existe
 if [ -f "$1" ]; then
-  echo "O arquivo já existe!"
-  exit 1
+    echo "O arquivo já existe!"
+    exit 1
 fi
 
-
-# Oferece opção de escolha do editor de texto
 echo "Escolha um editor de texto:"
 select editor in "Nano" "VI"; do
-  case "$editor" in
-  Nano)
-    # Insere o cabeçalho, edita utilizando o nano e adiciona permissão de
-		# execução.
-    echo "$cabecalho" > "$1" && nano "$1" && chmod u+x "$1"
-    break
-    ;;
-  VI)
-    # Insere o cabeçalho, edita utilizando o vi e adiciona permissão de
-		# execução.
-    echo "$cabecalho" > "$1" && vi "$1" && chmod u+x "$1"
-    break
-    ;;
-  *)
-    echo "Opção inválida!"
-    exit 1
-    ;;
-  esac
+    case "$editor" in
+        Nano)
+            echo "$cabecalho" > "$1" && nano "$1" && chmod u+x "$1"
+            break
+            ;;
+        VI)
+            echo "$cabecalho" > "$1" && vi "$1" && chmod u+x "$1"
+            break
+            ;;
+        *)
+            echo "Opção inválida!"
+            exit 1
+            ;;
+    esac
 done
 
 echo -e "\nCabeçalho inserido e arquivo criado: $1"
--- scripts/salva-antes-de-editar.sh
+++ scripts/salva-antes-de-editar.sh
@@ -3,45 +3,56 @@
 # Github: https://github.com/jplfalcao
 # Descrição: Salva uma cópia antes de editar um arquivo
 # Data de criação: 15/02/2022
-# Data de modificação: 10/09/2023
-# Versão: 1.4
+# Data de modificação: 30/01/2024
+# Versão: 1.5
 # Uso: ./salva-antes-de-editar.sh <arquivo>
 
-# Verifica se foi passado algum parâmetro
 if [ "$#" -ne 1 ]; then
-  echo "Informe o nome do arquivo!"
-  exit 1
+    echo "Informe o nome do arquivo!"
+    exit 1
 fi
 
-arquivo="$1"
+if [ ! -e "$1" ]; then
+    echo "O arquivo \"$1\" não existe!"
 
-# Verifica se o arquivo NÃO existe
-if [ ! -e "$arquivo" ]; then
-  echo "O arquivo \"$arquivo\" não existe"
-  # Oferece opção de escolha
-  read -r -n 1 -p "Deseja criar? (s|n): " escolha
-  case "$escolha" in
-    [Ss])
-      touch "$arquivo" && echo -e "\nArquivo \"$arquivo\" criado!"
-      exit 1
-      ;;
-    [Nn])
-      echo -e "\nErro ao criar arquivo \"$arquivo\"!"
-      exit 1
-      ;;
-    *)
-      echo -e "Opção inválida!"
-      exit 1
-  esac
+    read -r -n 1 -p "Deseja criar? (s|n): " escolha
+    case "$escolha" in
+        [Ss])
+            touch "$1" && echo -e "\nArquivo \"$1\" criado!"
+            exit 1
+            ;;
+        [Nn])
+            echo -e "\nErro ao criar arquivo \"$1\"!"
+            exit 1
+            ;;
+        *)
+            echo -e "Opção inválida!"
+            exit 1
+    esac
 fi
 
-# Verifica se o arquivo possui permissão de escrita
-if [ ! -w "$arquivo" ]; then
-  echo "Você não tem permissão de escrita para o arquivo \"$arquivo\"!"
-  exit 1
+if [ ! -w "$1" ]; then
+    echo "Você não tem permissão de escrita para o arquivo \"$1\"!"
+    exit 1
 fi
 
-# Realizando a cópia
-cp -a "$arquivo" "$arquivo.bkp"
-vi "$arquivo"
+cp -a "$1" "$1.bkp"
+
+echo "Escolha um editor de texto:"
+select editor in "Nano" "VI"; do
+    case "$editor" in
+        Nano)
+            nano "$1"
+            break
+            ;;
+        VI)
+            vi "$1"
+            break
+            ;;
+        *)
+            echo "Opção inválida!"
+            exit 1
+            ;;
+    esac
+done
 
